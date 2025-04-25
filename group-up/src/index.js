0a7e0d9e90b2ec340d3e0e195fdd3dd2698b7bb8
epboblet <epboblet@asu.edu>
2025-04-24 22:08:22 -0700

    updated page title

--- group-up/src/index.js
+++ group-up/src/index.js
@@ -12,13 +12,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
+  <>
+  <title>Coral</title>
+  <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
     </BrowserRouter>
+  </>
   // </React.StrictMode>
 );
 
